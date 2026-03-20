45c01b8885865130235aa4be6eabb901b0a89881
Nosima <breno.nosima@gmail.com>
2026-03-20 11:05:43 -0300

    first commit

---
+++ src/main/java/com/bn/gerenciamentodeautorescs/GerenciamentoDeAutoresCsApplication.java
@@ -0,0 +1,13 @@
+package com.bn.gerenciamentodeautorescs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeAutoresCsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciamentoDeAutoresCsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bn/gerenciamentodeautorescs/GerenciamentoDeAutoresCsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bn.gerenciamentodeautorescs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeAutoresCsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
